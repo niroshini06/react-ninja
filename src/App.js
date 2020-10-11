23325021f0c5fa81dad1ad45d66350fdd85bbc17
Vigneshwaran Chandrasekaran <ac.vigneshwaran@gmail.com>
2020-10-11 16:16:47 +0530

    nesting components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Ninjas from './Ninjas';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div>
         <h1>My first react app</h1>
         <p> welcome :) </p>
+        <Ninjas />
       </div>
     )
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
 
+class Ninjas extends Component {
+    render() {
+        return(
+            <div>
+                <div>Name: Ryu</div>
+                <div>Age: 30</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
