e259e5d6635ed6a581d6b8bdecc4862228d5b586
niroshini <niroshini06@gmail.com>
2020-10-14 18:46:05 +0530

    functions as props - passing data from child to paren comp

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import  StatelessComp from './statelessComp';
 import IfElseConditionalOutput from './IfElseConditionalOutput';
 import TernaryConditionalOutput from './TernaryConditionalOutput';
 import AddNinja from './AddNinja';
+import FunctionsAsProps from './FunctionsAsProps';
 
 class App extends Component {
   state = {
@@ -15,6 +16,14 @@ class App extends Component {
       {name: 'test2', age: 25, belt: 'blue', id:2},
       {name: 'test3', age: 26, belt: 'green', id:3},
     ]
+  }
+
+  addNinja = (newNinja) => {
+    newNinja.id = this.state.ninjas.length+1;
+   let newList = [...this.state.ninjas, newNinja];
+    this.setState({
+      ninjas: newList
+    })
   }
   render() {
     return (
@@ -30,10 +39,12 @@ class App extends Component {
 
         <IfElseConditionalOutput  ninjas={this.state.ninjas} /> 
 
-        <TernaryConditionalOutput  ninjas={this.state.ninjas} /> */}
+        <TernaryConditionalOutput  ninjas={this.state.ninjas} /> 
 
-        <AddNinja />
-         
+        <AddNinja /> */}
+
+        <FunctionsAsProps addNinja={ this.addNinja} />
+        <StatelessComp ninjas={this.state.ninjas} /> 
       </div>
     )
   }
